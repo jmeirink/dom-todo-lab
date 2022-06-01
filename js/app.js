f565e69f088e15b65f73874709db07b83395c1b6
Jesse Meirink <jesse@meirink.com>
2022-06-01 15:56:13 -0500

    complete project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!')
+const inp = document.querySelector('#userInput')
+const btn = document.querySelector('#submit-button')
+const list = document.querySelector('#todo-list')
+
+btn.addEventListener('click', function(evt){
+  const li = document.createElement('li')
+  li.textContent = inp.value
+  document.querySelector('ul').appendChild(li)
+  inp.value = ''
+})
